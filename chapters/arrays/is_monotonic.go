652a354e1a37255b0b9aaa1c403ecf5c26cf9f50
João Henri <joao.henri.cr@gmail.com>
2024-01-31 13:08:49 -0300

    [ARRAYS]: Add is monotonic problem

---
+++ chapters/arrays/is_monotonic.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func IsMonotonic(array []int) bool {
+	nonDecreasing := true
+	nonIncreasing := true
+
+	i := 0
+	for i < len(array)-1 {
+		if array[i+1] < array[i] {
+			nonDecreasing = false
+		}
+		if array[i+1] > array[i] {
+			nonIncreasing = false
+		}
+		i++
+	}
+
+	return nonIncreasing || nonDecreasing
+
+}
+
+func main() {
+	fmt.Println(IsMonotonic([]int{-1, -5, -10, -1100, -1100, -1101, -1102, -9001}))
+}
