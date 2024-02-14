bfaefdd2fea51b0c587940ad1bf35288dacfe453
João Henri <joao.henri@ifood.com.br>
2024-02-13 21:08:25 -0300

    [TREEs]: Invert binary tree

---
+++ chapters/trees/invert_tree.go
@@ -0,0 +1,38 @@
+package main
+
+func (bt *BinaryTree) InvertBinaryTree() {
+	if bt == nil {
+		return
+	}
+
+	if bt.Left == nil && bt.Right == nil {
+		return
+	}
+
+	if bt.Left != nil {
+		bt.Left.InvertBinaryTree()
+	}
+
+	if bt.Right != nil {
+		bt.Right.InvertBinaryTree()
+	}
+
+	rightTree := bt.Right
+	leftTree := bt.Left
+
+	bt.Right = leftTree
+	bt.Left = rightTree
+}
+
+func main() {
+	tree := &BinaryTree{Value: 1}
+	tree.Left = &BinaryTree{Value: 2}
+	tree.Right = &BinaryTree{Value: 3}
+	tree.Left.Left = &BinaryTree{Value: 4}
+	tree.Left.Right = &BinaryTree{Value: 5}
+	tree.Left.Left.Left = &BinaryTree{Value: 8}
+	tree.Left.Left.Right = &BinaryTree{Value: 9}
+	tree.Right.Left = &BinaryTree{Value: 6}
+	tree.Right.Right = &BinaryTree{Value: 7}
+	tree.InvertBinaryTree()
+}
--- chapters/trees/nodedepths.go
+++ chapters/trees/nodedepths.go
@@ -1,9 +1,5 @@
 package main
 
-import (
-	"fmt"
-)
-
 type BinaryTree struct {
 	Value       int
 	Left, Right *BinaryTree
@@ -35,15 +31,15 @@ func (bt *BinaryTree) NodeDepthsRec(currentDepth int) int {
 	return currentDepth + leftSum + rightSum
 }
 
-func main() {
-	root := &BinaryTree{Value: 1}
-	root.Left = &BinaryTree{Value: 2}
-	root.Left.Left = &BinaryTree{Value: 4}
-	root.Left.Left.Left = &BinaryTree{Value: 8}
-	root.Left.Left.Right = &BinaryTree{Value: 9}
-	root.Left.Right = &BinaryTree{Value: 5}
-	root.Right = &BinaryTree{Value: 3}
-	root.Right.Left = &BinaryTree{Value: 6}
-	root.Right.Right = &BinaryTree{Value: 7}
-	fmt.Println(NodeDepths(root))
-}
+//func main() {
+//	root := &BinaryTree{Value: 1}
+//	root.Left = &BinaryTree{Value: 2}
+//	root.Left.Left = &BinaryTree{Value: 4}
+//	root.Left.Left.Left = &BinaryTree{Value: 8}
+//	root.Left.Left.Right = &BinaryTree{Value: 9}
+//	root.Left.Right = &BinaryTree{Value: 5}
+//	root.Right = &BinaryTree{Value: 3}
+//	root.Right.Left = &BinaryTree{Value: 6}
+//	root.Right.Right = &BinaryTree{Value: 7}
+//	fmt.Println(NodeDepths(root))
+//}
